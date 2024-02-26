768408e4b03e2d88ce8ac81a2568997c5b9d6228
Carlos Ravelo <ravelo.carlos@gmail.com>
2024-02-26 11:12:36 -0500

    Update 2-apps.sh

--- sources/2-apps.sh
+++ sources/2-apps.sh
@@ -2,15 +2,7 @@
 
 pinfo "Installing Required Packages"
 brew bundle --file=- <<EOF
-    # tap "homebrew/aliases"
-    # tap "homebrew/autoupdate"
-    # tap "homebrew/bundle"
-    # tap "homebrew/cask"
-    # tap "homebrew/cask-versions"
-    # tap "homebrew/core"
-    # tap "homebrew/services"
     tap "fluxcd/tap" # fpr flux the gitops engine
-    #tap "tomanthony/brews" # tap fpr itermocil
     tap "homebrew/command-not-found" # https://github.com/Homebrew/homebrew-command-not-found when a command is not found, search brew for the formula
     tap "rs/tap"
 
@@ -20,10 +12,9 @@ brew bundle --file=- <<EOF
     brew 'mackup'         # App configuration service
 
     # Shell Stuff
-    cask 'iterm2'         # Doing things on a terminal
+    cask 'warp'           # Doing things on a terminal
     brew 'bash'           # macos version is different
     brew 'zsh'            # Not bash
-    #brew 'TomAnthony/brews/itermocil' # iTerm2 layouts
 
     # Generally useful things
     brew 'coreutils'      # All those cool GNU things
@@ -50,13 +41,14 @@ brew bundle --file=- <<EOF
     cask 'visual-studio-code' # Best quick editor
 
     # Development
-    cask 'docker'          # Doing things in containers
-    cask 'virtualbox'      # Doing things in VMs
+    # cask 'docker'          # Doing things in containers
+    cask 'rancher' # Container manager
+    cask 'virtualbox'      # Doing things in VMs # not compatible with Apple Silicon :scream:
     brew 'kubernetes-cli'  # Kubernetes
     brew 'kubernetes-helm' # Helm: the package manager for k8s
     brew "kubectx"         # change context easier
     brew "kustomize"       # kubernetes yaml helper
-    brew 'helmfile'        # Kubernetes helm automation
+    # brew 'helmfile'        # Kubernetes helm automation
     brew 'fluxcd/tap/flux' # GitOps engine
     brew 'octant'          # Kubernetes dashboard
     cask 'gas-mask'        # manage hosts files
@@ -64,12 +56,10 @@ brew bundle --file=- <<EOF
     cask 'postman'         # RESTful calls
     brew 'vegeta'          # Its over 9000!!!!!! Load testing tool
     cask 'balenaetcher'    # For making usb flash from images
-    cask 'tunnelblick'     # Client for OpenVPN
-    brew 'node'            # Javascript!
-    brew 'nvm'             # Javascript Versions
+    brew 'nvm'             # Javascript
     brew 'yarn'            # Javascript deps
     brew 'go'              # Not Javascript also Not c++ :)
-    brew "nginx"           # Reverse proxy
+    # brew "nginx"           # Reverse proxy
     
     # The CLOUUUUD!!!
     cask 'google-cloud-sdk' # google
@@ -81,7 +71,6 @@ brew bundle --file=- <<EOF
     cask 'firefox'         # Browsing the web
     cask "microsoft-edge"  # Browsing the web
     brew 'screen'          # Just in case
-    cask 'appcleaner'      # Help removing stuff
     cask "recordit"        # Screen recordings
     cask "the-unarchiver"  # Uncompress anything
 
@@ -89,13 +78,14 @@ brew bundle --file=- <<EOF
     cask 'steam'          # So I can play games
 
     # Pretty
-    cask 'aerial' # best screensaver
+    # cask 'aerial' # best screensaver # osx has it installed now
 
     # Apple Store Apps
     brew 'mas'
     mas 'Magnet', id: 441258766
     mas 'Microsoft Remote Desktop', id: 1295203466
-    mas "Sequel Ace", id: 1518036000
+    mas 'Sequel Ace', id: 1518036000
+    mas 'Bitwarden', id: 1352778147
 EOF
 
 brew cleanup
